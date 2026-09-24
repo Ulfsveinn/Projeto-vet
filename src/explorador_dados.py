e262179b846892753fbb30ccfa9c3ae751b1e489
André Luis Mesquita Barbosa Júnior <andremesquitabarbosa@gmail.com>
2026-09-24 14:37:18 -0300

    teste

--- src/explorador_dados.py
+++ src/explorador_dados.py
@@ -4,6 +4,8 @@ pd.set_option('display.max_columns', None)
 
 df = pd.read_csv("dataraw/wter-evkm_version_57907.csv")
 
+print(df.columns.tolist())
+
 
 
 
@@ -14,7 +16,7 @@ vet_df = df.rename(columns={'datatime2' : 'data_e_hora_2', 'datatime' : 'data_e_
 
 
 
-#vet_df.info()
+vet_df.info()
 
 vet_df.copy()
 
@@ -29,18 +31,31 @@ vet_df.copy()
 
 
 
-for colunas in vet_df.select_dtypes(include='object').columns:
+for colunas in vet_df.select_dtypes(include='str').columns:
    vet_df[colunas] = vet_df[colunas].str.strip()
 
 
 
 print(pd.isnull(vet_df).sum().tolist())
 
+values = {'nome': 'Desconhecido', 'Local encontrado': 'Desconhecido', 'tipo_de_entrada': 'Desconhecido', 'condicao_de_entrada': 'Desconhecido', 'tipo_de_animal': 'Desconhecido', 'sexo_na_entrada': 'Desconhecido', 'idade_na_entrada': 'Desconhecido', 'raca': 'Desconhecido', 'cor': 'Desconhecido'}
 
-vet_df = vet_df.dropna(how='any', axis=0)
+vet_df = vet_df.fillna(value=values)
+print(pd.isnull(vet_df).sum().tolist())
 
 vet_df = vet_df.drop_duplicates()
 
+print(vet_df.duplicated().sum())
 
 
-print(pd.isnull(vet_df).sum().tolist())
+
+vet_df[['numero_idade', 'unidade_idade']] = vet_df['idade_na_entrada'].str.split(' ', expand=True)
+
+
+print(vet_df[['idade_na_entrada', 'numero_idade', 'unidade_idade']].head(10))
+
+
+print(vet_df['unidade_idade'].unique())
+vet_df['numero_idade'] = vet_df['numero_idade'].pd.to_numeric(errors='coerce')
+vet_df = vet_df[vet_df['numero_idade'] < 0]
+
